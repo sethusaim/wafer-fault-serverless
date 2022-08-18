7d1eb52551b2ab689afa4a6c5ccb069e5c71b705
sethusaim <sethusaim@gmail.com>
2022-08-18 12:49:03 +0530

    improvised step_function.tf

--- infrastructure/step_functions/train/step_function.tf
+++ infrastructure/step_functions/train/step_function.tf
@@ -1,17 +1,9 @@
 resource "aws_sfn_state_machine" "train_state_machine" {
   name       = "train-state-machine"
   role_arn   = aws_iam_role.step_function_lambda_invole_role.arn
-  definition = <<EOF
-{
-  "Comment": "A Hello World example of the Amazon States Language using an AWS Lambda Function",
-  "StartAt": "HelloWorld",
-  "States": {
-    "HelloWorld": {
-      "Type": "Task",
-      "Resource": "arn:aws:lambda:us-east-1:347460842118:function:test",
-      "End": true
-    }
-  }
+  definition = data.local_file.train_step_function_file.content
 }
-EOF
+
+data "local_file" "train_step_function_file" {
+  filename = "./data/step_functions/train_step_function.json"
 }
