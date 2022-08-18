5ad8166ca6fe2428a5653b897299efb2888f40d6
sethusaim <sethusaim@gmail.com>
2022-08-18 12:48:39 +0530

    improvised step_function_execution_role.tf

--- infrastructure/step_functions/train/step_function_execution_role.tf
+++ infrastructure/step_functions/train/step_function_execution_role.tf
@@ -1,72 +1,16 @@
 resource "aws_iam_role" "step_function_lambda_invole_role" {
   name               = "step_function_lambda_invole_role"
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "states.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+  assume_role_policy = data.local_file.step_function_lambda_invole_role_file.content
 }
 
 resource "aws_iam_policy" "step_function_lambda_invoke_policy" {
-  name = "step_function_lambda_invoke_policy"
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "lambda:InvokeFunction"
-          ],
-          "Resource" : [
-            "arn:aws:lambda:us-east-1:347460842118:function:test:*"
-          ]
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "lambda:InvokeFunction"
-          ],
-          "Resource" : [
-            "arn:aws:lambda:us-east-1:347460842118:function:test"
-          ]
-        }
-      ]
-    }
-  )
+  name   = "step_function_lambda_invoke_policy"
+  policy = data.local_file.step_function_lambda_invoke_policy_file.content
 }
 
-
 resource "aws_iam_policy" "step_function_xray_access_policy" {
-  name = "step_function_xray_access_policy"
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Effect" : "Allow",
-          "Action" : [
-            "xray:PutTraceSegments",
-            "xray:PutTelemetryRecords",
-            "xray:GetSamplingRules",
-            "xray:GetSamplingTargets"
-          ],
-          "Resource" : [
-            "*"
-          ]
-        }
-      ]
-    }
-  )
+  name   = "step_function_xray_access_policy"
+  policy = data.local_file.step_function_xray_access_policy_file.content
 }
 
 resource "aws_iam_policy_attachment" "step_function_lambda_invoke_policy_attachment" {
@@ -79,4 +23,16 @@ resource "aws_iam_policy_attachment" "step_function_xray_access_policy_attachmen
   name       = "step_function_xray_access_policy_attachment"
   roles      = [aws_iam_role.step_function_lambda_invole_role.name]
   policy_arn = aws_iam_policy.step_function_xray_access_policy.arn
+}
+
+data "local_file" "step_function_lambda_invole_role_file" {
+  filename = "./data/roles/train_step_function_lambda_invole_role.json"
+}
+
+data "local_file" "step_function_xray_access_policy_file" {
+  filename = "./data/policies/train_step_function_xray_access_policy.json"
+}
+
+data "local_file" "step_function_lambda_invoke_policy_file" {
+  filename = "./data/policies/train_step_function_lambda_invoke_policy.json"
 }
