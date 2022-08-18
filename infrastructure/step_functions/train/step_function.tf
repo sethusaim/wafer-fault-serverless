dd0939ab3463448485c8c3c68d12cfbb6c3377e5
sethusaim <sethusaim@gmail.com>
2022-08-18 16:02:08 +0530

    restructured train step functions

--- infrastructure/step_functions/train/step_function.tf
+++ infrastructure/step_functions/train/step_function.tf
@@ -1,7 +1,15 @@
 resource "aws_sfn_state_machine" "train_state_machine" {
-  name       = "train-state-machine"
+  name       = var.train_state_machine_name
   role_arn   = aws_iam_role.step_function_lambda_invole_role.arn
   definition = data.local_file.train_step_function_file.content
+}
+
+data "local_file" "step_function_lambda_invole_role_file" {
+  filename = "./data/roles/step_function_lambda_invole_role.json"
+}
+
+data "local_file" "step_function_xray_access_policy_file" {
+  filename = "./data/policies/step_function_xray_access_policy.json"
 }
 
 data "local_file" "train_step_function_file" {
---
+++ infrastructure/step_functions/train/variables.tf
@@ -0,0 +1,29 @@
+variable "step_function_lambda_invole_role_name" {
+  default = "step_function_lambda_invole_role"
+  type    = string
+}
+
+variable "step_function_lambda_invoke_policy_name" {
+  default = "step_function_lambda_invoke_policy"
+  type    = string
+}
+
+variable "step_function_xray_access_policy_name" {
+  default = "step_function_xray_access_policy"
+  type    = string
+}
+
+variable "step_function_lambda_invoke_policy_attachment_name" {
+  default = "step_function_lambda_invoke_policy_attachment"
+  type    = string
+}
+
+variable "step_function_xray_access_policy_attachment_name" {
+  default = "step_function_xray_access_policy_attachment"
+  type    = string
+}
+
+variable "train_state_machine_name" {
+  default = "train-state-machine"
+  type    = string
+}
