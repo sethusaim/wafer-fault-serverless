6a50c28e594af60cca6dc1512ce9aa8ab8d9b0a7
sethusaim <sethusaim@gmail.com>
2022-08-18 13:11:15 +0530

    added variables to pred step function module

--- infrastructure/step_functions/prediction/step_function.tf
+++ infrastructure/step_functions/prediction/step_function.tf
@@ -1,5 +1,5 @@
-resource "aws_sfn_state_machine" "train_state_machine" {
-  name       = "pred-state-machine"
+resource "aws_sfn_state_machine" "pred_state_machine" {
+  name       = var.pred_state_machine_name
   role_arn   = aws_iam_role.step_function_lambda_invole_role.arn
   definition = data.local_file.train_step_function_file.content
 }
--- infrastructure/step_functions/prediction/step_function_execution_role.tf
+++ infrastructure/step_functions/prediction/step_function_execution_role.tf
@@ -1,26 +1,26 @@
 resource "aws_iam_role" "step_function_lambda_invole_role" {
-  name               = "step_function_lambda_invole_role"
+  name               = var.step_function_lambda_invole_role_name
   assume_role_policy = data.local_file.step_function_lambda_invole_role_file.content
 }
 
 resource "aws_iam_policy" "step_function_lambda_invoke_policy" {
-  name   = "step_function_lambda_invoke_policy"
+  name   = var.step_function_lambda_invoke_policy_name
   policy = data.local_file.pred_step_function_lambda_invoke_policy_file.content
 }
 
 resource "aws_iam_policy" "step_function_xray_access_policy" {
-  name   = "step_function_xray_access_policy"
+  name   = var.step_function_xray_access_policy_name
   policy = data.local_file.step_function_xray_access_policy_file.content
 }
 
 resource "aws_iam_policy_attachment" "step_function_lambda_invoke_policy_attachment" {
-  name       = "step_function_lambda_invoke_policy_attachment"
+  name       = var.step_function_lambda_invoke_policy_attachment_name
   roles      = [aws_iam_role.step_function_lambda_invole_role.name]
   policy_arn = aws_iam_policy.step_function_lambda_invoke_policy.arn
 }
 
 resource "aws_iam_policy_attachment" "step_function_xray_access_policy_attachment" {
-  name       = "step_function_xray_access_policy_attachment"
+  name       = var.step_function_xray_access_policy_attachment_name
   roles      = [aws_iam_role.step_function_lambda_invole_role.name]
   policy_arn = aws_iam_policy.step_function_xray_access_policy.arn
 }
